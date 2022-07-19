85fd14a8a03b2e49edb9961bdbf47aa6f6ff7cce
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-07-20 08:02:47 +0900

    Fix to properly handle errors on CLI level

--- cmd/cloud-secrets-manager/main.go
+++ cmd/cloud-secrets-manager/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"os"
+
 	"github.com/h0n9/toybox/cloud-secrets-manager/cli"
 )
 
 func main() {
-	cli.RootCmd.Execute()
+	err := cli.RootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
 }
