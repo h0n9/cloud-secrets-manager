c1457c2eeebdf9a28d24554ffb8abdbdd39623b9
h0n9 <monsieur.ahn@gmail.com>
2024-04-17 16:52:37 +0900

    Add list command to CLI for listing secrets

---
+++ cli/secrets/list.go
@@ -0,0 +1,62 @@
+package secrets
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/h0n9/cloud-secrets-manager/provider"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "list secrets",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// define variables
+		var (
+			err            error
+			secretProvider provider.SecretProvider
+		)
+
+		// init ctx
+		ctx := context.Background()
+
+		// init secret provider
+		switch strings.ToLower(providerName) {
+		case "aws":
+			secretProvider, err = provider.NewAWS(ctx)
+		case "gcp":
+			secretProvider, err = provider.NewGCP(ctx)
+		default:
+			return fmt.Errorf("failed to figure out secret provider")
+		}
+		if err != nil {
+			return err
+		}
+		defer secretProvider.Close()
+
+		// list secrets
+		secrets, err := secretProvider.ListSecrets()
+		if err != nil {
+			return err
+		}
+
+		// print secrets
+		for _, secret := range secrets {
+			fmt.Println(secret)
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	listCmd.Flags().StringVar(
+		&providerName,
+		"provider",
+		DefaultProviderName,
+		"cloud provider name",
+	)
+}
