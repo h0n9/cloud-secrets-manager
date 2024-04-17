379fb22db99c20cd664c66364ad3e693427b9469
h0n9 <monsieur.ahn@gmail.com>
2024-04-17 11:02:01 +0900

    Add ListSecrets method to SecretProvider interface

--- provider/provider.go
+++ provider/provider.go
@@ -2,6 +2,7 @@ package provider
 
 type SecretProvider interface {
 	Close() error
+	ListSecrets() ([]string, error)
 	GetSecretValue(string) (string, error)
 	SetSecretValue(string, string) error
 }
