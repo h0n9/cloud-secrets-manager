8103e05a237e4fc967f0157653350e21016aff3b
h0n9 <monsieur.ahn@gmail.com>
2024-05-03 08:32:47 +0900

    Add int param to limit the number of secrets to ListSecrets()

--- provider/provider.go
+++ provider/provider.go
@@ -2,7 +2,7 @@ package provider
 
 type SecretProvider interface {
 	Close() error
-	ListSecrets() ([]string, error)
+	ListSecrets(int) ([]string, error)
 	GetSecretValue(string) (string, error)
 	SetSecretValue(string, string) error
 }
