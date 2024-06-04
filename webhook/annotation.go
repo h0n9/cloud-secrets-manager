d593d2b4b7f857b2165250b3e1a9ce5c0b046925
h0n9 <monsieur.ahn@gmail.com>
2024-06-05 08:50:51 +0900

    feat: Add base64 decoding for secret values in annotation.go

--- webhook/annotation.go
+++ webhook/annotation.go
@@ -9,21 +9,23 @@ import (
 )
 
 const (
-	AnnotationProvider = "provider"
-	AnnotationSecretID = "secret-id"
-	AnnotationTemplate = "template"
-	AnnotationOutput   = "output"
-	AnnotationInjected = "injected"
+	AnnotationProvider  = "provider"
+	AnnotationSecretID  = "secret-id"
+	AnnotationTemplate  = "template"
+	AnnotationOutput    = "output"
+	AnnotationDecodeB64 = "decode-base64"
+	AnnotationInjected  = "injected"
 )
 
 type AnnotationSet map[string]Annotations
 
 var AnnotationMap = map[string]string{
-	fmt.Sprintf("%s/%s", csm.AnnotationPrefix, AnnotationProvider): AnnotationProvider,
-	fmt.Sprintf("%s/%s", csm.AnnotationPrefix, AnnotationSecretID): AnnotationSecretID,
-	fmt.Sprintf("%s/%s", csm.AnnotationPrefix, AnnotationTemplate): AnnotationTemplate,
-	fmt.Sprintf("%s/%s", csm.AnnotationPrefix, AnnotationOutput):   AnnotationOutput,
-	fmt.Sprintf("%s/%s", csm.AnnotationPrefix, AnnotationInjected): AnnotationInjected,
+	fmt.Sprintf("%s/%s", csm.AnnotationPrefix, AnnotationProvider):  AnnotationProvider,
+	fmt.Sprintf("%s/%s", csm.AnnotationPrefix, AnnotationSecretID):  AnnotationSecretID,
+	fmt.Sprintf("%s/%s", csm.AnnotationPrefix, AnnotationTemplate):  AnnotationTemplate,
+	fmt.Sprintf("%s/%s", csm.AnnotationPrefix, AnnotationOutput):    AnnotationOutput,
+	fmt.Sprintf("%s/%s", csm.AnnotationPrefix, AnnotationDecodeB64): AnnotationDecodeB64,
+	fmt.Sprintf("%s/%s", csm.AnnotationPrefix, AnnotationInjected):  AnnotationInjected,
 }
 
 func ParseAnnotationSet(input map[string]string) AnnotationSet {
@@ -53,11 +55,12 @@ func ParseAnnotationSet(input map[string]string) AnnotationSet {
 type Annotations map[string]string
 
 var annotationsAvailable = map[string]bool{
-	AnnotationProvider: true,
-	AnnotationSecretID: true,
-	AnnotationTemplate: true,
-	AnnotationOutput:   true,
-	AnnotationInjected: true,
+	AnnotationProvider:  true,
+	AnnotationSecretID:  true,
+	AnnotationTemplate:  true,
+	AnnotationOutput:    true,
+	AnnotationDecodeB64: true,
+	AnnotationInjected:  true,
 }
 
 func ParseAndCheckAnnotations(input Annotations) Annotations {
@@ -109,4 +112,12 @@ func (a Annotations) GetTemplate() (string, error) {
 
 func (a Annotations) GetOutput() (string, error) {
 	return a.getValue(AnnotationOutput)
+}
+
+func (a Annotations) GetDecodeB64() (bool, error) {
+	value, err := a.getValue(AnnotationDecodeB64)
+	if err != nil {
+		return false, err
+	}
+	return strconv.ParseBool(value)
 }
